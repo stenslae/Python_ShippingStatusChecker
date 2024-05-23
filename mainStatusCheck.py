533f77b367dad225d31cedf8d40d6f1aded47556
stenslae <141681972+stenslae@users.noreply.github.com>
2024-05-23 13:34:32 -0600

    Update mainStatusCheck.py

--- mainStatusCheck.py
+++ mainStatusCheck.py
@@ -1,10 +1,12 @@
-import siteScraping
+from statusCheckPackage import siteScraping
 import time
 
 #################TODO: USER UPDATES THIS INFO
 inputfile = "orders.csv"
-datapath = r"user\file\path"
-chromedriverpath = r"user\file\path"
+datapath = r"\userpickanyfilepath"
+chromedriverpath = r"\pathtochromedriver"
+username = r'amazon_email'
+password = r'amazon_password'
 ############################
 
 # Read file and return array with carrier name, tracking number, and row number
@@ -14,17 +16,26 @@ data = siteScraping.inforead(inputfile)
 file = open('statusCheckErrors.txt', 'w')
 
 print("Checking shipping statuses...")
+#Log into amazon and open webpage to browse for statuses
+driver = siteScraping.amazonlogin(username, password, file, datapath, chromedriverpath)
+
 # Check statuses of data
 statuses = []
 undelivered = []
 for i in range(len(data)):
-    delivered = siteScraping.checkstatus(data[i][0], data[i][1], data[i][2], file, datapath, chromedriverpath)
-    status = [data[i
-              ][2], delivered]
+    delivered = siteScraping.checkstatus(data[i][0], data[i][1], data[i][2], file, driver)
+    status = [data[i][2], delivered]
     statuses.append(status)
     if 'Delivered' not in delivered:
         undelivered.append(data[i][2])
-    time.sleep(2)  # Delay between requests to avoid being blocked
+    print(f'Row {data[i][2]} has been checked')
+    time.sleep(5)  # Delay between requests to avoid being blocked
+
+# Clear browsing data (history, cookies, cache, etc.)
+driver.execute_script("window.localStorage.clear();")
+driver.execute_script("window.sessionStorage.clear();")
+driver.delete_all_cookies()
+driver.quit()
 
 print("All shipping statues have been checked.")
 # Load arrays onto csv file to flag undelivered rows, and add row of statuses
